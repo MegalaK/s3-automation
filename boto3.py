a507484385e29b3097a38fabc45342d4f187032a
MegalaK <32275855+MegalaK@users.noreply.github.com>
2023-10-16 13:39:52 -0400

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -1,2 +1,39 @@
 import pandas as pd
 import logging
+import os
+import numpy as np
+import boto3
+from io import StringIO
+import pprint
+import shutil
+import time
+import sys
+from datetime import datetime, timezone
+import readProperties
+from pypdf import PdfReader
+import re
+
+session=boto3.Session(profile_name='RT')
+s3_client=session.client('athena')
+
+pdf_path='c:\\Users\\megala\\Documents\\Quick_Automat.pdf'
+Config=readProperties.Readconfig
+achive_path=Config.getData("Queries","Archive_path")
+output_path=Config.getData("Queries","html_path")
+Temp_query=Config.getData("Queries","selected_values")
+
+if Temp_query == 'provider_view':
+  header='count'
+  merge='header'
+  comp1='count'
+  comp2='count_id'
+  section="<h2>Provider Views & Pro
+
+
+
+
+
+
+
+
+
